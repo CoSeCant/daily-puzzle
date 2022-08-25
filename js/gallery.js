eaae2eec8157b142cbab0b264c1055436a1669fc
CoSeCant <1collinscam@gmail.com>
2022-08-24 21:01:49 -0400

    I have returned the files to the state where none of the images load due to the source pathway issue so that I can figure out a way to fix that while also keeping the lightbox arrow functionality intact (which currently seems to be breaking with the current attempt at fixing the source pathway!

--- js/gallery.js
+++ js/gallery.js
@@ -11,7 +11,7 @@ for (i = 1; i <= numNFTs; i++) {
 }
 console.log(NFTs)
 
-
+/*
 //When uploading the NFTs from the above array to github, there was a problem since the source pathway to the images was only for the local machine. Using the images in the array below should solve this as the sourcing pathway is more linear. However, we still need to keep the above array in order for the lightbox arrow functionalty to work (since when searching the array for the NFT, the source pathway needs to be formatted in the way that the NFTs from the 'NFTs' array is, and not the way the 'sourcedNFTs' array does it)
 const sourcedNFTs = []
 
@@ -19,4 +19,4 @@ for (j = 1; j <= numNFTs; j++) {
     sourcedNFTs.push('./NFT/' + j + '.jpg')
 }
 
-console.log(sourcedNFTs)
+console.log(sourcedNFTs)*/
